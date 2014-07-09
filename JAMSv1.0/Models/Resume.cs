7a44f0581dc58106652d722bb562e94bcec15c4e
dae023 <daek023@aol.com>
2014-07-09 15:10:17 -0700

    checkingViewRemoval

--- JAMSv1.0/Models/Resume.cs
+++ JAMSv1.0/Models/Resume.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,11 @@ namespace JAMSv1._0.Models
 {
     public class Resume
     {
+        [Key]
+        public int ResumeId { get; set; }
+
         public HttpPostedFileBase File  { get; set; }
+
     }
     //Test Mensur Hussien
     //Test Mo Moorthy
