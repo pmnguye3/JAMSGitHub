694be81d5bf45ff1ebcf753d16a71cc43672debc
dae023 <daek023@aol.com>
2014-07-23 23:06:28 -0700

    AddClasses

---
+++ JAMSv1.0/Models/Quiz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JAMSv1._0.Models
+{
+    public class Quiz
+    {
+        public List<Question> Questions { set; get; }
+
+        public int rightAnswers = 0;
+
+        public Quiz()
+        {
+            Questions = new List<Question>();
+        }
+    }
+}
