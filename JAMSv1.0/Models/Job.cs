6beda3365cef7a341ee125300ca0d7ad45517b6c
dae023 <daek023@aol.com>
2014-07-31 11:41:30 -0700

    AddedJobClass

---
+++ JAMSv1.0/Models/Job.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JAMSv1._0.Models
+{
+    /// <summary>
+    /// Job class for new job postings by administrator
+    /// </summary>
+    public class Job
+    {
+        /// <summary>
+        /// primary key for tracking jobs
+        /// </summary>
+        [Required]
+        public int JobId { get; set; }
+
+        /// <summary>
+        /// Name of the job 
+        /// </summary>
+        [Display(Name = "Position")]
+        [Required]
+        public string JobName { get; set; }
+
+        /// <summary>
+        /// Describes the job duties and responsibilities
+        /// </summary>
+        [Display(Name = "Description")]
+        [Required]
+        public string JobDescription { get; set; }
+
+        /// <summary>
+        /// Lets the user know who posted the job
+        /// </summary>
+        [Display(Name = "Posted By")]
+        [Required]
+        public string JobPostedBy { get; set; }
+
+        /// <summary>
+        /// Date the position was made available
+        /// </summary>
+        [Display(Name = "Position Posted On")]
+        [Required]
+        public DateTime JobStartDate { get; set; }
+
+    }
+}
