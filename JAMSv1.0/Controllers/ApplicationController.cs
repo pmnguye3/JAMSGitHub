6abac385ff41adc0c177b982f2bc2254d4d20510
dae023 <daek023@aol.com>
2014-07-31 13:41:18 -0700

    RestoreFirstLastName

---
+++ JAMSv1.0/Controllers/ApplicationController.cs
@@ -0,0 +1,38 @@
+﻿using JAMSv1._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace JAMSv1._0.Controllers
+{
+    /// <summary>
+    /// Controller to setup the first and last name to display at login
+    /// </summary>
+    public abstract class ApplicationController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (User != null)
+            {
+                var context = new ApplicationDbContext();
+                var username = User.Identity.Name;
+
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var user = context.Users.SingleOrDefault(u => u.UserName == username);
+                    string fullName = string.Concat(new string[] { user.FirstName, " ", user.LastName });
+                    ViewData.Add("FullName", fullName);
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
+
+        public ApplicationController()
+        {
+
+        }
+    }
+}
